b47e6a4cef8fa4b1d27338093ce94f8143df8396
Palak <Palakgoyal0304@gmail.com>
2023-10-02 15:04:41 +0530

    Add add to fav button on display page

--- src/Components/Gallery/GalleryComponent.jsx
+++ src/Components/Gallery/GalleryComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import "./GalleryComponent.css"
 import { db } from "../../lib/firebase"
 import isOwner from "../../js/Owner"
@@ -6,11 +6,36 @@ import AddWork from "./AddWork"
 import Loader from "../Loader/Loader"
 import { useNavigate } from "react-router-dom"
 
+import { BsSuitHeart, BsSuitHeartFill } from "react-icons/bs";
+
 
 const GalleryComponent = () => {
     const [addWork, setAddWork] = useState(false)
-    const [workDocuments, setWorkDocuments] = useWorkData();
+    const [workDocuments, setWorkDocuments, isFetching] = useWorkData();
+    const [favArr, setFavArr] = useState([])
     const navigate = useNavigate();
+
+
+    useEffect(() => {
+        const localFavArr = localStorage.getItem("favArr");
+        const tempArr = localFavArr ? JSON.parse(localFavArr) : [];
+        setFavArr(tempArr)
+    }, [])
+
+    function handleAddToFav(id) {
+        setFavArr(prev => [...prev, id])
+        localStorage.setItem("favArr", JSON.stringify(favArr))
+    }
+
+    function handleRemoveFav(id) {
+        const idx = favArr.indexOf(id);
+        if (idx === -1) return;
+
+        const tempArr = [...favArr];
+        tempArr.splice(idx, 1);
+        setFavArr(tempArr)
+        localStorage.setItem("favArr", JSON.stringify(tempArr))
+    }
 
     return (
         <div className="gallery_container">
@@ -28,19 +53,30 @@ const GalleryComponent = () => {
                     Work
                 </h2>
             </div>
-            {workDocuments? (
-            <div className="display_img_container">
-                {workDocuments.map(doc => (
-                    <div className="display_img_box" key={doc.id}>
-                        <img
-                            src={doc.images[0]}
-                            alt={doc.name}
-                            className="display_img gallery_image"
-                            onClick={() => navigate(doc.id)}
-                        />
-                    </div>
-                ))}
-            </div>
+            {!isFetching ? (
+                <div className="display_img_container">
+                    {workDocuments.map(doc => (
+                        <div className="gallery_image_container display_img_box" key={doc.id}>
+                            <img
+                                src={doc.images[0]}
+                                alt={doc.name}
+                                className="display_img gallery_image"
+                                onClick={() => navigate(doc.id)}
+                            />
+                            {favArr.includes(doc.id) ? (
+                                <BsSuitHeartFill
+                                    onClick={() => handleRemoveFav(doc.id)}
+                                    className="display_img_set_fav_btn popup"
+                                />
+                            ) : (
+                                <BsSuitHeart
+                                    onClick={() => handleAddToFav(doc.id)}
+                                    className="display_img_set_fav_btn popup"
+                                />
+                            )}
+                        </div>
+                    ))}
+                </div>
             ) : (
                 <Loader />
             )}
@@ -54,6 +90,7 @@ export default GalleryComponent
 
 function useWorkData() {
     const [workDocuments, setWorkDocuments] = useState([]);
+    const isFetchingRef = useRef(true)
 
     useEffect(() => {
         // Define a reference to your Firestore collection
@@ -65,10 +102,10 @@ function useWorkData() {
                 data.push({ id: doc.id, ...doc.data() });
             });
             setWorkDocuments(data);
-        });
-
+        })
+        isFetchingRef.current = false;
         return () => { unsubscribe() };
     }, []);
 
-    return [workDocuments, setWorkDocuments]
+    return [workDocuments, setWorkDocuments, isFetchingRef.current]
 }
