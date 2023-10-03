c8b80e533fdd3cf38c63490dede324b96f1b4805
Palak <Palakgoyal0304@gmail.com>
2023-10-03 15:10:07 +0530

    Add read more button on about page

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -2,8 +2,11 @@ import "./About.css"
 import ActionBtn from '../ActionBtn/ActionBtn'
 import { ownerImg } from '../../assets'
 import FAQ from "../FAQ/FAQ"
+import { useState } from "react"
 
 const AboutComponent = () => {
+    const [isReadMore, setIsReadMore] = useState(false)
+
     return (
         <div>
             <div className="about_container">
@@ -17,25 +20,23 @@ const AboutComponent = () => {
                         Banasthali Vidyapith
                     </h3>
                     <p className="sub_para_styling about_para">
-                        Starting with a childhood dream, I've turned
-                        my passion into a powerful force. With faith,
-                        hard work, and a calm spirit, I've seen my
-                        creative endeavors flourish. The joy I find
-                        in my work is unmatched, driving me tirelessly
-                        forward. Choosing to follow my heart in
-                        pursuing my craft has been a game-changer.
-                        In just one year, my dreams have started to
-                        come true, thanks to @banasthali_vidyapith.
-                        I'm proud to be Vice Captain at the Art and
-                        Craft Club, a dream fulfilled. This role
-                        carries responsibility and a chance to
-                        share my skills and creativity. As I
-                        prepare for my first workshop, this moment
-                        is filled with meaning. It's just the
-                        beginning of an extraordinary journey.
+                        {aboutText}
                     </p>
                     <p className="about_para">
                         <br />
+                    </p>
+                    <p className="about_para_small sub_para_styling">
+                        {isReadMore? aboutText : aboutText.substring(0, 351)}
+                        {" "}
+                        {!isReadMore && (
+                            <span style={{ fontWeight: 500 }}>...</span>
+                        )}
+                        <button
+                            className="read_more_btn"
+                            onClick={() => setIsReadMore(prev => !prev)}
+                        >
+                            read {isReadMore? "less" : "more"}
+                        </button>
                     </p>
                     <div style={{ paddingTop: "16px" }}>
                         <ActionBtn text="Get In Touch" target="/contact" />
@@ -48,3 +49,21 @@ const AboutComponent = () => {
 }
 
 export default AboutComponent
+
+
+const aboutText = `Starting with a childhood dream, I've turned
+                    my passion into a powerful force. With faith,
+                    hard work, and a calm spirit, I've seen my
+                    creative endeavors flourish. The joy I find
+                    in my work is unmatched, driving me tirelessly
+                    forward. Choosing to follow my heart in
+                    pursuing my craft has been a game-changer.
+                    In just one year, my dreams have started to
+                    come true, thanks to @banasthali_vidyapith.
+                    I'm proud to be Vice Captain at the Art and
+                    Craft Club, a dream fulfilled. This role
+                    carries responsibility and a chance to
+                    share my skills and creativity. As I
+                    prepare for my first workshop, this moment
+                    is filled with meaning. It's just the
+                    beginning of an extraordinary journey.`
