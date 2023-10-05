de1e1c8c3295079d1c9b92d794c88d13206f245b
Palak <Palakgoyal0304@gmail.com>
2023-10-05 14:49:42 +0530

    Add social embed

--- src/Components/Hero/MoreAboutUs.jsx
+++ src/Components/Hero/MoreAboutUs.jsx
@@ -9,8 +9,9 @@ const MoreAboutUs = () => {
                         Follow Us On:
                     </h2>
                 </div>
-                <div>
-
+                <div className="iframe_container">
+                    <iframe src='https://widgets.sociablekit.com/linkedin-profile-posts/iframe/204773' frameborder='0'  className="linkedin_iframe" scrolling='no'></iframe>
+                    <iframe src='https://widgets.sociablekit.com/instagram-feed/iframe/204793' frameborder='0' className="linkedin_iframe" scrolling='no'></iframe>
                 </div>
             </div>
         </div>
@@ -18,3 +19,6 @@ const MoreAboutUs = () => {
 }
 
 export default MoreAboutUs
+
+// https://www.linkedin.com/in/mahi-desarla-7b4361266/
+// https://www.instagram.com/the_craft_galleryy/?igshid=YmMyMTA2M2Y%3D
